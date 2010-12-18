5ec8d002f5dd0261187b4392b92eb1dd14d5b5a9
pctao <pctao.git@pctao.tw>
2010-12-18 10:50:12 +0800

    init rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punch::Application.config.secret_token = '77e812f3ea5d01609aabe518d98f937c33ee2f03eb7ed1b03c596286eff17bb73c21496c322ef0011f5dc96a8d758e92d338861af73263db2ab9df884ab989dd'
